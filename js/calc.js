51576da6f557e06847a3746d1ef765278f6c238a
anaabelar <anaj.abelar@gmail.com>
2025-02-13 10:20:08 -0300

    Adicionando novos arquivos JS

---
+++ js/calc.js
@@ -0,0 +1,14 @@
+// Declaração de variável
+let num1 = prompt("Digite um número");
+let num2 = prompt("Digite outro número");
+
+//Operações matematicas
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+// Exibindo os resultados no console
+console.log("O resultado da soma é:" + soma);
+console.log("O resultado da subtração é:" + sub);
+console.log("O resultado da multiplicação é:" + mult);
+console.log("O resultado da divisão é:" + div);
