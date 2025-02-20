e134bd7fcb9f9353354411916e8c3ed251437a4b
anaabelar <anaj.abelar@gmail.com>
2025-02-20 08:44:28 -0300

    Adicionando variaveis

---
+++ front-20-02/variavel.js
@@ -0,0 +1,10 @@
+let nome = "Anaju"; //String
+let idade = 17; //Int
+let casado = false; //Booleano
+
+//Imprimindo variáveis
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof casado);
+
+//let pode alterar valor; const é imutavel
