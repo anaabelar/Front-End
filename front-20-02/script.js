bc2c9972871e6f64ad6fdaa28607b4fb9b86c416
anaabelar <anaj.abelar@gmail.com>
2025-02-20 09:50:18 -0300

    Adicionando função

---
+++ front-20-02/script.js
@@ -0,0 +1,14 @@
+function minhaFuncao() {
+    alert("Olá");
+}
+
+function minhaFuncao(nome, idade) {
+    console.log(nome + idade);
+}
+
+//Chamando função
+dados("Ana", 37);
+dados("Carla" + 40);
+dados("Juliana" + 8);
+dados("Doris" + 13);
+
